ea729edd75bcc7cc3db9b76241515dc5734de55f
Herry <herry13@gmail.com>
2018-06-03 00:58:13 +0100

    added skeleton of python files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="fruit-cli",
+    version="0.1.0",
+    author="Herry",
+    author_email="herry13@gmail.com",
+    packages=["fruitcli"],
+    #include_package_data=True,
+    url="https://github.com/fruit-testbed/fruit-cli/",
+    license="LICENSE.txt",
+    description="Command-line application of Fruit cluster management",
+    #install_requires=[],
+)
