dfb58229a31b99f5e66dce7dc6963f0c4820b444
Herry <herry13@gmail.com>
2018-06-07 22:54:57 +0100

    updated pip packaging setup files

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name="fruit-cli",
     version="0.1.0",
     author="Herry",
-    author_email="herry13@gmail.com",
-    packages=["fruitcli"],
-    #include_package_data=True,
+    author_email="herry.herry@glasgow.ac.uk",
+    license="Creative Commons Attribution-ShareAlike 4.0 International Public",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords="fruit-cli",
+    platforms="any",
+    packages=["fruit_cli"],
     url="https://github.com/fruit-testbed/fruit-cli/",
-    license="LICENSE.txt",
     description="Command-line application of Fruit cluster management",
-    #install_requires=[],
+    install_requires=["requests"],
+    python_requires=">=2.7, <4",
+    scripts=["bin/fruit-cli"],
 )
