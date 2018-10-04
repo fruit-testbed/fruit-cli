0d493b44905be9be27def7139f719dfd4ed7412e
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-10-04 18:58:45 +0100

    Identity check token_tool option

--- fruit/auth/token_tool.py
+++ fruit/auth/token_tool.py
@@ -6,23 +6,31 @@ import fruit.auth.ssh_key
 import fruit.auth.signify_key
 
 def _main():
-    (_program, filename) = sys.argv
-    if hasattr(sys.stdin, 'buffer'):
-        password = sys.stdin.buffer.readline().rstrip(b'\n')
-    else:
-        password = sys.stdin.readline().rstrip(b'\n')
-
-    try:
-        sk = fruit.auth.ssh_key.SshPrivateKey(filename)
-        sk.unprotect(password)
-    except:
+    if len(sys.argv) == 1:
+        token = sys.stdin.readline()
         try:
-            sk = fruit.auth.signify_key.SignifyPrivateKey(filename)
-            sk.unprotect(password)
+            print(fruit.auth._b64(fruit.auth.authenticated_identity(token)))
         except:
             sys.exit(1)
-    identity = sk.public_key
-    print(fruit.auth.make_authenticated_identity(identity, sk.signer_for_identity(identity)))
+    else:
+        filename = sys.argv[1]
+        if hasattr(sys.stdin, 'buffer'):
+            password = sys.stdin.buffer.readline().rstrip(b'\n')
+        else:
+            password = sys.stdin.readline().rstrip(b'\n')
+
+        try:
+            sk = fruit.auth.ssh_key.SshPrivateKey(filename)
+            sk.unprotect(password)
+        except:
+            try:
+                sk = fruit.auth.signify_key.SignifyPrivateKey(filename)
+                sk.unprotect(password)
+            except:
+                sys.exit(1)
+        identity = sk.public_key
+        print(fruit.auth.make_authenticated_identity(identity, sk.signer_for_identity(identity)))
+
     sys.exit(0)
 
 if __name__ == '__main__':
